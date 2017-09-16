6e075bf82b76897d86e4a521de26e3150a2194ff
tunglam123 <minkboy96@gmail.com>
2017-09-16 05:58:52 -0700

    Add files via upload

---
+++ BT-HW/BT2-fix/common.js
@@ -0,0 +1,9 @@
+function Validate() {
+        var password = document.getElementById("txtPassword").value;
+        var confirmPassword = document.getElementById("txtConfirmPassword").value;
+        if (password != confirmPassword) {
+            alert("Passwords do not match.");
+            return false;
+        }
+        return true;
+    }
